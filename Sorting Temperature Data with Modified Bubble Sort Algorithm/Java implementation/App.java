e3234335298965dbaaf682066d89f363087bd2f0
unknown <amirrizashibly1382@gmail.com>
2024-03-27 10:47:12 +0330

    The java implementation of the Modified Bubble Sort Algorithm for sorting temperature data was added.

---
+++ Sorting Temperature Data with Modified Bubble Sort Algorithm/Java implementation/App.java
@@ -0,0 +1,40 @@
+package y;
+
+
+public class App 
+{
+    public static int[] bubbleSortTemperatures(int[] temperatureData) {
+        int n = temperatureData.length;
+        int stage = 0;
+
+        for (int i = 0; i < n; i++) {
+            boolean swapped = false;
+            for (int j = 1; j < n; j++) {
+                if (temperatureData[j] < temperatureData[j - 1]) {
+                    int temp = temperatureData[j];
+                    temperatureData[j] = temperatureData[j - 1];
+                    temperatureData[j - 1] = temp;
+                    swapped = true;
+                    stage++;
+                }
+            }
+            if (!swapped) {
+                break;
+            }
+        }
+
+        System.out.println("Algorithm stopped at stage " + stage);
+        return temperatureData;
+    }
+
+    public static void main( String[] args )
+    {
+        int[] temperatureData = {-7, -12, 24, 15, 3, -1, 14, 6};
+        int[] sortedTemperatureData = bubbleSortTemperatures(temperatureData);
+
+        System.out.print("Sorted temperature data: ");
+        for (int temperature : sortedTemperatureData) {
+            System.out.print(temperature + " ");
+    }
+}
+}
