25edc7ed9665b149b97fc123bfdb7e8df3af98c2
unknown <amirrizashibly1382@gmail.com>
2024-04-13 23:12:46 +0330

    Fixing a bug.

--- Sorting Temperature Data with Modified Bubble Sort Algorithm/Python implementation/bubble_sort.py
+++ Sorting Temperature Data with Modified Bubble Sort Algorithm/Python implementation/bubble_sort.py
@@ -1,6 +1,5 @@
 def bubble_sort_temperatures(temperature_data):
     n = len(temperature_data)
-    stage = 0
 
     for i in range(n):
         swapped = False
@@ -8,16 +7,15 @@ def bubble_sort_temperatures(temperature_data):
             if temperature_data[j] > temperature_data[j + 1]:
                 temperature_data[j], temperature_data[j + 1] = temperature_data[j + 1], temperature_data[j]
                 swapped = True
-                stage += 1
         if not swapped:
+            print(i)
+            print("Algorithm stopped at stage", i)
             break
 
-    print("Algorithm stopped at stage", stage)
     return temperature_data
 
 
-
-temperature_data = [-7, -12, 24, 15, 3, -1, 14, 6]
+temperature_data = [-7, 12, 24, 15, 3, -1, 14, 6]
 sorted_temperature_data = bubble_sort_temperatures(temperature_data)
 print("Sorted temperature data:", sorted_temperature_data)
 
